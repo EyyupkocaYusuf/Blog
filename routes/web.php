b65137a2dbfee9c06916db287aeb32eabf746293
laravel <EyyupkocaYusuff@gmail.com>
2023-03-21 19:21:58 +0300

    admin panel and Login view page completed.

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Backend
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+*/
+
+Route::get('/admin/dashboard',[\App\Http\Controllers\backend\Dashboard::class,'index'])->name('admin.dashboard');
+Route::get('/admin/giris',[\App\Http\Controllers\backend\Auth::class,'login'])->name('admin.login');
+/*
+|--------------------------------------------------------------------------
+| Web Front
+|--------------------------------------------------------------------------
 */
 
 Route::get('/',[\App\Http\Controllers\Front\HomepageController::class,'index'])->name('homepage');
